365fc4d108f3c2dd767030ba3ff9ab33b0e2b9c0
francescacl <fracl.carpineta@gmail.com>
2023-11-28 13:48:45 +0100

    Update data acquisition from serial port

--- src/air_quality_sensor_driver/include/topic_air_quality_sensor/pub.hpp
+++ src/air_quality_sensor_driver/include/topic_air_quality_sensor/pub.hpp
@@ -13,6 +13,45 @@
 
 #define PUB_PERIOD 300
 
+
+class SerialComm
+{
+public:
+  SerialComm(std::string port, unsigned int baud_rate)
+  : io(), serial(io, port)
+  {
+    serial.set_option(asio::serial_port_base::baud_rate(baud_rate));
+  }
+
+  bool get_data(std::vector<std::string>& str_vector)
+  {
+    asio::streambuf buf;
+    asio::read_until(serial, buf, '\n');
+    std::istream stream(&buf);
+
+    std::string received_data;
+    std::getline(stream, received_data);
+
+    // Save all the acquired tokens in a vector
+    std::stringstream str_stream(received_data);
+    std::string token;
+    bool none = false;
+    while (std::getline(str_stream, token, ',')) {
+      str_vector.push_back(token);
+      if (token == std::string(" None")){
+        none = true;
+      }
+    }
+
+    return none;
+  }
+
+private:
+  asio::io_service io;
+  asio::serial_port serial;
+};
+
+
 class Pub : public rclcpp::Node
 {
 public:
@@ -23,12 +62,30 @@ private:
   rclcpp::Publisher<traxxas1_interfaces::msg::AirQuality>::SharedPtr air_quality_pub_;
   rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr gps_pub_;
 
-  std::shared_ptr<asio::serial_port> port_;
+  std::unique_ptr<SerialComm> serial;
 
   // rclcpp::TimerBase::SharedPtr OBJ;
   rclcpp::TimerBase::SharedPtr pub_timer_; // timer to publish messages
   void pub_timer_callback(void);
   unsigned long pub_cnt_;
+
+  bool connected = false;
+
+  int device_id;
+  std::string acquisition_date;
+  float latitude;
+  float longitude;
+  float altitude;
+  int gps_precision;
+  float pm25;
+  float pm10;
+  float humidity;
+  float temperature;
+  float no2;
+  float co2;
+  float nh3;
+  float co;
 };
+
 
 #endif
--- src/air_quality_sensor_driver/src/pub.cpp
+++ src/air_quality_sensor_driver/src/pub.cpp
@@ -20,6 +20,9 @@ Pub::Pub()
     "/car1/gps/position",
     rclcpp::QoS(10));
 
+  serial = std::make_unique<SerialComm>("/dev/air_quality_sensor", 9600);
+  connected = true;
+
   // std::chrono::duration (period)
   pub_timer_ = this->create_wall_timer(
     std::chrono::milliseconds(PUB_PERIOD), // 300 milliseconds, period
@@ -34,113 +37,94 @@ Pub::Pub()
 // Publishes a message on timer occurrence
 void Pub::pub_timer_callback(void)
 {
+  if (connected)
+  {
+    try {
 
-  try {
+      // Read from serial port
+      std::vector<std::string> str_vector;
+      bool none = serial->get_data(str_vector);
 
-    int device_id;
-    std::string acquisition_date;
-    float latitude;
-    float longitude;
-    float altitude;
-    int gps_precision;
-    float pm25;
-    float pm10;
-    float humidity;
-    float temperature;
-    float no2;
-    float co2;
-    float nh3;
-    float co;
+      if(str_vector.size() == 14 && !none) {
 
-    // Read from serial port
-    
-    asio::io_context io;
-    asio::serial_port port(io, "/dev/ttyACM1"); // TODO: add udev rule
+        // Save the acquired values into the appropriate variables
+        device_id = std::stoi(str_vector[0]);
+        acquisition_date = str_vector[1];
+        latitude = std::stof(str_vector[2]);
+        longitude = std::stof(str_vector[3]);
+        altitude = std::stof(str_vector[4]);
+        gps_precision= std::stoi(str_vector[5]);
+        pm25 = std::stof(str_vector[6]);
+        pm10 = std::stof(str_vector[7]);
+        humidity = std::stof(str_vector[8]);
+        temperature = std::stof(str_vector[9]);
+        no2 = std::stof(str_vector[10]);
+        co2 = std::stof(str_vector[11]);
+        nh3 = std::stof(str_vector[12]);
+        co = std::stof(str_vector[13]);
 
-    asio::streambuf buf;
-    asio::read_until(port, buf, '\n');
-    std::istream stream(&buf);
+        std::cout << std::endl;
+        RCLCPP_INFO(get_logger(), "New data acquisition");
+        std::cout << "\ndevice_id: " << device_id << std::endl;
+        std::cout << "acquisition_date: " << acquisition_date << std::endl;
+        std::cout << "\nlatitude: " << latitude << std::endl;
+        std::cout << "longitude: " << longitude << std::endl;
+        std::cout << "altitude: " << altitude << std::endl;
+        std::cout << "gps_precision: " << gps_precision << std::endl;
+        std::cout << "\npm25: " << pm25 << std::endl;
+        std::cout << "pm10: " << pm10 << std::endl;
+        std::cout << "humidity: " << humidity << std::endl;
+        std::cout << "temperature: " << temperature << std::endl;
+        std::cout << "no2: " << no2 << std::endl;
+        std::cout << "co2: " << co2 << std::endl;
+        std::cout << "nh3: " << nh3 << std::endl;
+        std::cout << "co: " << co << std::endl;
 
-    std::string received_data;
-    std::getline(stream, received_data);
+        // Messages
 
-    // Save all the acquired tokens in a vector
-    std::stringstream str_stream(received_data);
-    std::string token;
-    std::vector<std::string> str_vector;
-    bool none = false;
-    while (std::getline(str_stream, token, ',')) {
-      str_vector.push_back(token);
-      if (token == std::string(" None")){
-        none = true;
+        sensor_msgs::msg::NavSatFix new_msg_gps{};
+        new_msg_gps.set__longitude(longitude);
+        new_msg_gps.set__latitude(latitude);
+        new_msg_gps.set__altitude(altitude);
+        gps_pub_->publish(new_msg_gps);
+
+        traxxas1_interfaces::msg::AirQuality new_msg_air{};
+        new_msg_air.set__device_id(device_id);
+        new_msg_air.set__acquisition_date(acquisition_date);
+        new_msg_air.set__gps_data(new_msg_gps);
+        new_msg_air.set__gps_precision(gps_precision);
+        new_msg_air.set__pm25(pm25);
+        new_msg_air.set__pm10(pm10);
+        new_msg_air.set__humidity(humidity);
+        new_msg_air.set__temperature(temperature);
+        new_msg_air.set__no2(no2);
+        new_msg_air.set__co2(co2);
+        new_msg_air.set__nh3(nh3);
+        new_msg_air.set__co(co);
+        air_quality_pub_->publish(new_msg_air);
+
+        pub_cnt_++;
+        std::cout << std::endl;
+        RCLCPP_INFO(this->get_logger(), "Published message %lu", pub_cnt_);
+        
       }
+    } catch (std::exception &exception) {
+      RCLCPP_ERROR(get_logger(), "Error in reading from serial port: %s", exception.what());
+      connected = false;
     }
 
-    if(str_vector.size() == 14 && !none) {
-
-      // Save the acquired values into the appropriate variables
-      device_id = std::stoi(str_vector[0]);
-      acquisition_date = str_vector[1];
-      latitude = std::stof(str_vector[2]);
-      longitude = std::stof(str_vector[3]);
-      altitude = std::stof(str_vector[4]);
-      gps_precision= std::stoi(str_vector[5]);
-      pm25 = std::stof(str_vector[6]);
-      pm10 = std::stof(str_vector[7]);
-      humidity = std::stof(str_vector[8]);
-      temperature = std::stof(str_vector[9]);
-      no2 = std::stof(str_vector[10]);
-      co2 = std::stof(str_vector[11]);
-      nh3 = std::stof(str_vector[12]);
-      co = std::stof(str_vector[13]);
-
-      std::cout << std::endl;
-      RCLCPP_INFO(get_logger(), "New data acquisition");
-      std::cout << "\ndevice_id: " << device_id << std::endl;
-      std::cout << "acquisition_date: " << acquisition_date << std::endl;
-      std::cout << "\nlatitude: " << latitude << std::endl;
-      std::cout << "longitude: " << longitude << std::endl;
-      std::cout << "altitude: " << altitude << std::endl;
-      std::cout << "gps_precision: " << gps_precision << std::endl;
-      std::cout << "\npm25: " << pm25 << std::endl;
-      std::cout << "pm10: " << pm10 << std::endl;
-      std::cout << "humidity: " << humidity << std::endl;
-      std::cout << "temperature: " << temperature << std::endl;
-      std::cout << "no2: " << no2 << std::endl;
-      std::cout << "co2: " << co2 << std::endl;
-      std::cout << "nh3: " << nh3 << std::endl;
-      std::cout << "co: " << co << std::endl;
-
-      // Messages
-
-      sensor_msgs::msg::NavSatFix new_msg_gps{};
-      new_msg_gps.set__longitude(longitude);
-      new_msg_gps.set__latitude(latitude);
-      new_msg_gps.set__altitude(altitude);
-      gps_pub_->publish(new_msg_gps);
-
-      traxxas1_interfaces::msg::AirQuality new_msg_air{};
-      new_msg_air.set__device_id(device_id);
-      new_msg_air.set__acquisition_date(acquisition_date);
-      new_msg_air.set__gps_data(new_msg_gps);
-      new_msg_air.set__gps_precision(gps_precision);
-      new_msg_air.set__pm25(pm25);
-      new_msg_air.set__pm10(pm10);
-      new_msg_air.set__humidity(humidity);
-      new_msg_air.set__temperature(temperature);
-      new_msg_air.set__no2(no2);
-      new_msg_air.set__co2(co2);
-      new_msg_air.set__nh3(nh3);
-      new_msg_air.set__co(co);
-      air_quality_pub_->publish(new_msg_air);
-
-      pub_cnt_++;
-      std::cout << std::endl;
-      RCLCPP_INFO(this->get_logger(), "Published message %lu", pub_cnt_);
-
+  }
+  else
+  {
+    try {
+      serial = std::make_unique<SerialComm>("/dev/airQualitySensor", 9600);
+      RCLCPP_INFO(get_logger(), "Reconnected");
     }
-  } catch (std::exception &exception) {
-    RCLCPP_ERROR(get_logger(), "Error in reading from serial port: %s", exception.what());
+    catch (std::exception &exception) {
+      RCLCPP_INFO(get_logger(), "Reconnection failed. Try again...");
+      return;
+    }
+    connected = true;
   }
   
 }
